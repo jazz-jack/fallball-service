35053ee3a7b2d9a47fb04f3cf3ccfb7dd9e12a26
Eduard Romanovskyj <eromanovskyj@odin.comj>
2016-09-16 14:32:55 +0300

    jenkins.sh fixes

--- jenkins.sh
+++ jenkins.sh
@@ -7,7 +7,10 @@ python3.4 -m flake8
 virtualenv -p python3.4 env
 . env/bin/activate
 
+pip install -r requirements.txt
 pip install -r test-requirements.txt
+mv fallball/fallball/settings_fallball.py fallball/fallball/settings.py
+
 python setup.py flake8
 python setup.py test
 
