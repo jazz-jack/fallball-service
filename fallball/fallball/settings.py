705d6f069b343513e81588d4a66314ff5c4961ca
Eduard Romanovskyj <eromanovskyj@odin.comj>
2016-10-11 19:19:54 +0300

    increase JWT token lifetime

--- fallball/fallball/settings.py
+++ fallball/fallball/settings.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -151,6 +153,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': timedelta(seconds=7200),
+}
 
 REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
