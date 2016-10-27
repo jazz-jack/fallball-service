60cc8b1ac09a8408458f89ec6eb2ced4d824fc5c
Eduard Romanovskyj <eromanovskyj@odin.comj>
2016-10-27 15:51:29 +0300

    For debugging purposes

--- fallball/fallballapp/middleware.py
+++ fallball/fallballapp/middleware.py
@@ -18,33 +18,31 @@ class RequestLogMiddleware(object):
         app_id = None
         reseller_name = None
 
-        try:
-            if reverse('v1:resellers-list') in request.path:
-                obj = get_model_object(request.user)
-                app_id = get_application_of_object(obj).id
+        if reverse('v1:resellers-list') in request.path:
+            obj = get_model_object(request.user)
+            app_id = get_application_of_object(obj).id
 
-                if request.resolver_match.url_name == 'resellers-detail':
-                    reseller_name = request.resolver_match.kwargs['name']
-                elif request.resolver_match.url_name != 'resellers-list':
-                    reseller_name = request.resolver_match.kwargs['reseller_name']
+            if request.resolver_match.url_name == 'resellers-detail':
+                reseller_name = request.resolver_match.kwargs['name']
+            elif request.resolver_match.url_name != 'resellers-list':
+                reseller_name = request.resolver_match.kwargs['reseller_name']
 
-            log = {'request': {}, 'response': {}}
+        log = {'request': {}, 'response': {}}
 
-            if app_id:
-                log['response'] = {'app': app_id, }
-            if reseller_name:
-                log['reseller'] = reseller_name
-            if response.content and response['content-type'] == 'application/json':
-                log['response']['body'] = json.loads(response.content.decode('utf-8'))
+        if app_id:
+            log['response'] = {'app': app_id, }
+        if reseller_name:
+            log['reseller'] = reseller_name
+        if response.content and response['content-type'] == 'application/json':
+            log['response']['body'] = json.loads(response.content.decode('utf-8'))
 
-            log['request']['headers'] = {
-                'CONTENT_TYPE': request.META['CONTENT_TYPE'],
-                'REQUEST_METHOD': request.META['REQUEST_METHOD'],
-            }
+        log['request']['headers'] = {
+            'CONTENT_TYPE': request.META['CONTENT_TYPE'],
+            'REQUEST_METHOD': request.META['REQUEST_METHOD'],
+        }
 
-            log['response']['headers'] = response._headers
+        log['response']['headers'] = response._headers
 
-            logger.info(json.dumps(log))
+        logger.info(json.dumps(log))
 
-        finally:
-            return response
+        return response
