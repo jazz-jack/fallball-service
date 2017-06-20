ac2bd92ffaf1fc0328f0ca88b93769687ba91127
Maxim Belov <mbelov@odin.com>
2017-06-20 17:21:14 +0300

    Minor formatting update

--- fallball/fallballapp/views.py
+++ fallball/fallballapp/views.py
@@ -26,8 +26,7 @@ from fallballapp.middleware import logger
 from fallballapp.models import Application, Client, ClientUser, Reseller, UNLIMITED
 from fallballapp.renderers import PlainTextRenderer
 from fallballapp.serializers import (ApplicationSerializer, ApplicationPutSerializer,
-                                     ClientSerializer,
-                                     ClientUserSerializer, ResellerSerializer,
+                                     ClientSerializer, ClientUserSerializer, ResellerSerializer,
                                      ResellerNameSerializer, UserAuthorizationSerializer)
 from fallballapp.utils import (get_app_username, get_object_or_403, get_jwt_token,
                                is_superuser, is_application, get_user_context, free_space)
