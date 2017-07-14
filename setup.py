20903ca3976073caf961f477c0094c689c539ad3
Maxim Belov <mbelov@odin.com>
2017-07-14 12:15:26 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(join(dirname(abspath(__file__)), 'requirements.txt'), 'r') as requirem
 
 setup(
     name='fallball',
-    version='1.1.7',
+    version='1.1.8',
     author='APS Connect team',
     author_email='aps@odin.com',
     packages=find_packages('fallball'),
