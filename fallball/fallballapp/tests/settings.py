12c91bc0162a8d3d2d103acd66df6e72cdf6a8d2
Mike Salnikov <msalnikov@odin.com>
2017-10-17 17:12:37 +0300

    add remove explicit collation/charset

--- fallball/fallballapp/tests/settings.py
+++ fallball/fallballapp/tests/settings.py
@@ -11,3 +11,19 @@ class DisableMigrations(object):
 
 MIGRATION_MODULES = DisableMigrations()
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fallball',
+        'USER': 'root',
+        'PORT': '3306',
+        'PASSWORD': '',
+        'HOST': 'fbdb',
+        'OPTIONS': {
+            'init_command': 'SET character_set_server="utf8", collation_server="utf8_unicode_ci"',
+        },
+        'TEST': {
+            'NAME': 'fallball_test'
+        }
+    }
+}
